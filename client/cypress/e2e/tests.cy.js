a838c0f1b8d082d3142f12c3a0073b379b404244
Izzy Figeuroa <izzyfigueroa6@gmail.com>
2024-11-21 15:45:37 -0500

    finished e2e testing

---
+++ client/cypress/e2e/tests.cy.js
@@ -0,0 +1,18 @@
+describe('Site Tests', () => {
+  it('passes', () => {
+    cy.visit('http://127.0.0.1:3000/')
+
+    // Step 3: Click the "Start Quiz" button
+    cy.get('button').contains('Start Quiz').click();
+    cy.get('button').contains('1').click()
+    cy.get('button').contains('3').click()
+    cy.get('button').contains('4').click()
+    cy.get('button').contains('2').click()
+    cy.get('button').contains('1').click()
+    cy.get('button').contains('4').click()
+    cy.get('button').contains('1').click()
+    cy.get('button').contains('3').click()
+    cy.get('button').contains('2').click()
+    cy.get('button').contains('1').click()
+  })
+  })
